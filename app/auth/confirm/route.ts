c5db1a9c98edefd3620db3f4e3f1313e24096c81
Joshua Emmanuel <joshuaemmanuel925@gmail.com>
2024-08-18 15:11:16 +0100

    Debugging...

--- app/auth/confirm/route.ts
+++ app/auth/confirm/route.ts
@@ -5,7 +5,7 @@ import { createClient } from '@/utils/supabase/server';
 import { redirect } from 'next/navigation';
 
 export async function GET(request: NextRequest) {
-  const { searchParams } = new URL(request.url);
+  const { searchParams, origin, pathname } = new URL(request.url);
   const token_hash = searchParams.get('token_hash');
   const type = searchParams.get('type') as EmailOtpType | null;
   const next = searchParams.get('next') ?? '/';
@@ -19,7 +19,16 @@ export async function GET(request: NextRequest) {
     });
     if (!error) {
       // redirect user to specified redirect URL or root of app
-      redirect(next);
+      // redirect(next);
+      searchParams.delete('token_hash');
+      searchParams.delete('type');
+
+      // Construct the clean URL without the token hash
+      const cleanUrl = `${origin}${pathname}?${searchParams.toString()}`;
+
+      // Redirect to the clean URL without keeping the old URL in history
+      redirect(cleanUrl || next);
+      return;
     }
   }
 
