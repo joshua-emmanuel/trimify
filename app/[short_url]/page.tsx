99c0f4d194cdbfabd4f213bbb8f0a8e09fb80521
Joshua Emmanuel <joshuaemmanuel925@gmail.com>
2024-08-21 14:10:07 +0100

    Handle 404 error when short link is not found

--- app/[short_url]/page.tsx
+++ app/[short_url]/page.tsx
@@ -2,18 +2,20 @@
 
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
+import { useToast } from '@/components/ui/use-toast';
 
-interface RedirectPageProps {
+type UrlType = {
+  original_url: string;
+};
+
+export default function RedirectPage({
+  params,
+}: {
   params: {
     short_url: string;
   };
-}
-
-interface UrlProps {
-  original_url: string;
-}
-
-export default function RedirectPage({ params }: RedirectPageProps) {
+}) {
+  const { toast } = useToast();
   const router = useRouter();
   const { short_url } = params;
 
@@ -21,7 +23,25 @@ export default function RedirectPage({ params }: RedirectPageProps) {
     const fetchUrl = async () => {
       try {
         const response = await fetch(`/api/shorten?short_url=${short_url}`);
-        const data: UrlProps = await response.json();
+        if (!response.ok) {
+          if (response.status === 404) {
+            toast({
+              variant: 'error',
+              title: 'Short Url Not Found',
+              description: 'You will be redirected to the homepage shortly',
+            });
+            router.push('/');
+            return;
+          }
+          toast({
+            variant: 'error',
+            title: 'An error occured',
+            description: 'You will be redirected to the homepage shortly',
+          });
+          router.push('/');
+          return;
+        }
+        const data: UrlType = await response.json();
         window.location.href = data.original_url;
       } catch (error) {
         router.push('/');
